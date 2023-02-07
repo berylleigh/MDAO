74502dfa6d63924d16e1ee5812b422629ae9b6ec
Beryl <jjjunkie@yahoo.com>
2023-02-08 01:11:30 +1000

    make cliphash as StorageMap key

--- pallets/clipbet2/src/lib.rs
+++ pallets/clipbet2/src/lib.rs
@@ -1,6 +1,4 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-
-
 pub use pallet::*;
 
 #[cfg(test)]
@@ -11,8 +9,6 @@ pub use pallet::*;
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
-// use frame_support::inherent::Vec;
-
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -33,45 +29,27 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
-
-	// pub fn encode_like<T: Encode, R: EncodeLike<T>>(data: &R) {
-	// 	data.encode(); // Valid `T` encoded value.
-	// }
-	// #[derive( Encode, Decode, Clone, PartialEq, Default, TypeInfo)]
-	// pub struct ClipInfo {
-	// 	/// Username, stored as an array of bytes.
-	// 	pub cliphash: Vec<u8>,
-	// 	/// Numberid of the user.
-	// 	pub id: i64,
-	// 	pub description: Vec<u8>, 
-	// 	// The "About Me" section of the user
-	// 	// pub about_me: Vec<u8>,
-	// }
-
-	// impl core::ops::Deref for ClipInfo{ 
-	// 	type Target = (u8,i64,u8);
-	// 	fn deref(&self) -> &Self::Target { &self.0 }
-	// }
-
-	// impl EncodeLike<(u8,i64,u8)> for ClipInfo {}
-	// impl WrapperTypeEncode for ClipInfo {}
-
 	
-   
-	
+	#[derive( Encode, Decode, Clone, PartialEq, Default, TypeInfo)]
+	pub struct ClipInfo {
+		/// Username, stored as an array of bytes.
+		pub cliphash: Vec<u8>,
+		/// Numberid of the user.
+		pub id: i64,
+		pub description: Vec<u8>, 
+		// The "About Me" section of the user
+		// pub about_me: Vec<u8>,
+	}
+
 	pub type GroupIndex = u32;	
 
 	// The pallet's runtime storage items.
 	// https://docs.substrate.io/main-docs/build/runtime-storage/
-	#[pallet::storage]
-	#[pallet::getter(fn all_members)]
-	// pub(super) type AllMembers<T: Config> = StorageValue<_, Vec<T::AccountId>, ValueQuery>;
-	pub(super) type AllMembers<T: Config> = StorageValue<_, Vec<Vec<u8>>, ValueQuery>;
-
+	
 	#[pallet::storage]
 	#[pallet::getter(fn info)]
 	// pub type AccountToClipInfo<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, Vec<u8>, OptionQuery>;
-	pub type AccountToClipInfo<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, Vec<Vec<u8>>, OptionQuery>;
+	pub type AccountToClipInfo<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, Vec<T::AccountId>,  OptionQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn member_score)]
@@ -96,9 +74,6 @@ pub mod pallet {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		ClipStored (T::AccountId, Vec<u8>),
-		GotStuff (T::AccountId),
-		NewMember(T::AccountId, Vec<u8>),
-
 		/// Put member score (id, index, score)
 		MemberJoinsGroup(T::AccountId, GroupIndex, u32),
 			
@@ -116,47 +91,20 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 
-
 		#[pallet::call_index(0)]
-		/// Join the `AllMembers` vec before joining a group
-		#[pallet::weight(10_000)]
-		pub fn join_all_members(origin: OriginFor<T>, cliphash: Vec<u8>,) -> DispatchResultWithPostInfo {
-			let new_member = ensure_signed(origin)?;
-			ensure!(
-				// !Self::is_member(&new_member),
-				!Self::is_member(&cliphash),
-				"already a member, can't join"
-			);
-			// <AllMembers<T>>::append(&new_member);
-			<AllMembers<T>>::append(&cliphash);
-
-			Self::deposit_event(Event::NewMember(new_member, cliphash));
-			Ok(().into())
-		}
-
-
-
-		#[pallet::call_index(1)]
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
-		//  pub fn register_user(origin: OriginFor<T>, cliphash: Vec<u8>, id: i64, description: Vec<u8>)->
 		
 		pub fn post_clip(
 			origin: OriginFor<T>, 
 			cliphash: Vec<u8>,
-			// )-> DispatchResultWithPostInfo {
 			)-> DispatchResult {	
 			let sender = ensure_signed(origin)?;
-			
-			<AccountToClipInfo<T>>::append(&sender, &cliphash);
-		
+			<AccountToClipInfo<T>>::append(&cliphash, &sender,);
 			Self::deposit_event(Event::<T>::ClipStored(sender, cliphash));
-			
-
-			// Ok(().into()) // used with -> DispatchResultWithPostInfo
 			Ok(()) // used with -> DispatchResult
 		}
 
-		#[pallet::call_index(2)]
+		#[pallet::call_index(1)]
 		#[pallet::weight(10_000)]
 		pub fn join_a_doublemap(
 			origin: OriginFor<T>,
@@ -164,48 +112,14 @@ pub mod pallet {
 			score: u32,
 		) -> DispatchResultWithPostInfo {
 			let member = ensure_signed(origin)?;
-			// ensure!(Self::is_member(&member), "not a member, can't remove");
 			<MemberScoreDmap<T>>::append(&member, &index, score);
-			
 			Self::deposit_event(Event::MemberJoinsGroup(member, index, score));
 			Ok(().into())
 		}
 
-		// #[pallet::call_index(1)]
-		// #[pallet::weight(10_000)]
-		// //  pub fn register_user(origin: OriginFor<T>, cliphash: Vec<u8>, id: i64, about_me: Vec<u8>)->
-		
-		// pub fn get_clip_stuff(
-		// 	origin: OriginFor<T>,
-		// 	// cliphash: Vec<u8>, 
-		// 	// description: Vec<u8>
-		// // )-> DispatchResult{
-		// )-> DispatchResultWithPostInfo {
-		// 	let sender = ensure_signed(origin)?;
-			
-		// 	let mut stuff: PrefixIterator<(Vec<u8>, Vec<u8>)> = <AccountToClipInfo<T>>::iter_prefix(&sender);
-			
-		// 	Self::deposit_event(Event::<T>::GotStuff(stuff.next()));
-		// 	// Self::deposit_event(Event::<T>::GotStuff(stuff.count()));
-			
-
-
-		// 	Ok(().into())
-		// }
-
-		
+				
 	}
 	
 }
 
-use frame_support::inherent::Vec;
-
-impl<T: Config> Pallet<T> {
-	
-	// for fast membership checks (see check-membership recipe for more details)
-	// fn is_member(who: &T::AccountId) -> bool {
-	fn is_member(who: &Vec<u8>) -> bool {	
-		Self::all_members().contains(who)
-	}
-}
 
